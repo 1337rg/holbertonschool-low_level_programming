8125980c9daf00ea79793786a416609428f82291
1337rg <remi.garzoli@protonmail.com>
2025-07-24 09:17:31 +0000

    day 4

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,43 +8,40 @@
  */
 int _atoi(char *s)
 {
-    int i = 0, sign = 1, res = 0, found_digit = 0;
-    int digit;
+	int i = 0, sign = 1, res = 0, found_digit = 0;
+	int digit;
 
 
-    while (s[i] && (s[i] < '0' || s[i] > '9'))
-    {
-        if (s[i] == '-')
-            sign = -sign;
-        else if (s[i] == '+')
-        {
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign = -sign;
+		else if (s[i] == '+')
+		{
+		}
+		i++;
+	}
 
-        }
-        i++;
-    }
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		found_digit = 1;
+		digit = s[i] - '0';
 
+		if (res > (2147483647 - digit) / 10)
+		{
+			/* Overflow detected */
+			if (sign == 1)
+				return (2147483647);
+			else
+				return (-2147483648);
+		}
 
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        found_digit = 1;
-        digit = s[i] - '0';
+		res = res * 10 + digit;
+		i++;
+	}
 
+	if (!found_digit)
+		return (0);
 
-        if (res > (2147483647 - digit) / 10)
-        {
-
-            if (sign == 1)
-                return (2147483647);
-            else
-                return (-2147483648); 
-        }
-
-        res = res * 10 + digit;
-        i++;
-    }
-
-    if (!found_digit)
-        return (0);
-
-    return (sign * res);
+	return (sign * res);
 }
