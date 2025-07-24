4e15b67c9e5dce82937ed31c8017c9f32c199eee
1337rg <remi.garzoli@protonmail.com>
2025-07-24 09:15:35 +0000

    day 4

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,22 +8,43 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, result = 0;
-	int found_digit = 0;
+    int i = 0, sign = 1, res = 0, found_digit = 0;
+    int digit;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			found_digit = 1;
-			result = result * 10 + (s[i] - '0');
-		}
-		else if (found_digit)
-			break;
-		i++;
-	}
 
-	return (sign * result);
+    while (s[i] && (s[i] < '0' || s[i] > '9'))
+    {
+        if (s[i] == '-')
+            sign = -sign;
+        else if (s[i] == '+')
+        {
+
+        }
+        i++;
+    }
+
+
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        found_digit = 1;
+        digit = s[i] - '0';
+
+
+        if (res > (2147483647 - digit) / 10)
+        {
+
+            if (sign == 1)
+                return (2147483647);
+            else
+                return (-2147483648); 
+        }
+
+        res = res * 10 + digit;
+        i++;
+    }
+
+    if (!found_digit)
+        return (0);
+
+    return (sign * res);
 }
