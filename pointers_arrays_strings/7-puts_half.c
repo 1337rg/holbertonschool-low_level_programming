514e2ad363de1413221a84eecc96880a21805ac5
1337rg <remi.garzoli@protonmail.com>
2025-07-24 08:57:22 +0000

    day 4

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - Prints the second half of a string,
+ *            followed by a new line.
+ * @str: The input string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+    int len = 0, i;
+
+    /* Find length of the string */
+    while (str[len] != '\0')
+        len++;
+
+    /* Calculate the starting index for printing */
+    if (len % 2 == 0)
+        i = len / 2;
+    else
+        i = (len + 1) / 2;
+
+    /* Print from the calculated start to the end */
+    while (str[i] != '\0')
+    {
+        putchar(str[i]);
+        i++;
+    }
+
+    putchar('\n');
+}
+
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void rev_string(char *s);
+void print_array(int *a, int n);
 
 #endif
 
